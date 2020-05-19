ee54d6a432ca810181d345084611c82e0353028c
felixrubio <felix.rubio@asesormasmovil.es>
2020-05-19 16:06:17 +0200

    Primera subida

---
+++ src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "Hola %s %s";
+	private final AtomicLong counter = new AtomicLong();	
+
+	@GetMapping("/saluda")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = " a todos	") String name,
+			@RequestParam(value = "apellido") String apellido) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name, apellido));
+	}
+}
