d6f7bd2c4f9ed93b5602ca089e4b7e8fb0816d2e
felixrubio <felix.rubio@asesormasmovil.es>
2020-05-20 11:46:23 +0200

    Add new method to access to a remote url

--- src/main/java/com/example/kte/Constants.java
+++ src/main/java/com/example/kte/Constants.java
@@ -3,5 +3,13 @@ package com.example.kte;
 public class Constants {
 	
 	public static final String URL_TEST = "https://jsonplaceholder.typicode.com/todos/1";
-
+	
+	// Retrieve a random chuck joke in JSON format.
+	public static final String URL_JOKE_CHUCK_NORRIS = "https://api.chucknorris.io/jokes/random";
+	
+	// Retrieve a list of available categories (animal, celebrity, fashion, political, music, etc)
+	public static final String URL_JOKE_CHUCK_NORRIS_CATEGORIES = "https://api.chucknorris.io/jokes/categories";
+	
+	// Retrieve a random chuck norris joke from a given category.
+	public static final String URL_JOKE_CHUCK_NORRIS_BY_CATEGORY = "https://api.chucknorris.io/jokes/random?category={category}";
 }
